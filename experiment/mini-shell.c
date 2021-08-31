f68e6234e4f54cb522048b00d31256b332c53788
thiagohdaqw <thiagohdaqw@gmail.com>
2021-08-31 13:49:48 -0300

    refactoring

--- experiment/mini-shell.c
+++ experiment/mini-shell.c
@@ -9,69 +9,95 @@
 #include <sys/time.h>
 #include <signal.h>
 
-#define MAX_COMMAND_SIZE 256
+#define MAX_COMMAND_PATH_SIZE 256
 #define MAX_COMMAND_ARGS_SIZE 256
 
 struct timeval child_process_timer;
 
-void timer_start(struct timeval *timer) {
-  gettimeofday(timer, 0);
+typedef struct command
+{
+    char path[MAX_COMMAND_PATH_SIZE];
+    char args[MAX_COMMAND_ARGS_SIZE];
+}command_t;
+
+
+void timer_start(struct timeval *timer)
+{
+    gettimeofday(timer, 0);
 }
 
-double timer_get_elapsed_time(struct timeval * timer) {
-  struct timeval now;
-  struct timeval elapsed;
-  
-  timer_start(&now);
+double timer_get_elapsed_time(struct timeval *timer)
+{
+    struct timeval now;
 
-  long seconds = now.tv_sec - timer->tv_sec;
-  long microseconds = now.tv_usec - timer->tv_usec;
-  return seconds + microseconds*1e-6;
+    timer_start(&now);
+
+    long seconds = now.tv_sec - timer->tv_sec;
+    long microseconds = now.tv_usec - timer->tv_usec;
+    return seconds + microseconds * 1e-6;
 }
 
-void start_child_timer(int signal){
-  timer_start(&child_process_timer);
+void start_child_timer(int signal)
+{
+    timer_start(&child_process_timer);
 }
 
-int main() {
-  signal(SIGUSR1, start_child_timer);
-
-  int ps_status;
-  pid_t process;
-  char command[MAX_COMMAND_SIZE];
-  char command_args[MAX_COMMAND_ARGS_SIZE];
-
-  struct timeval parent_process_timer;
-  double child_elapsed_time, parent_elapsed_time;
-
-  timer_start(&parent_process_timer);
-  while (scanf("%s", command) != EOF) {
-    scanf("%s", command_args);
-
-    process = fork();
-
-    if (process > 0) {
-      wait(&ps_status);
-      child_elapsed_time = timer_get_elapsed_time(&child_process_timer);
-      printf("> Demorou %.1f segundos, retornou %d\n", 
-        child_elapsed_time, WEXITSTATUS(ps_status));
-    }
-    else if (process == 0) {
-      kill(getppid(), SIGUSR1);
-      int there_was_error = execl(command, command, command_args, NULL);
-      if (there_was_error) {
+void manage_child(command_t *command)
+{
+    kill(getppid(), SIGUSR1);
+    
+    int there_was_error = execl(command->path, command->path, command->args, NULL);
+    
+    if (there_was_error)
+    {
         printf("> Erro: %s\n", strerror(errno));
+        fflush(stdout);
         exit(errno);
-      }
     }
-    else {
-      printf("> Não foi possível criar um processo.\n");
-      exit(1);
-    }
-  }
+}
 
-  parent_elapsed_time = timer_get_elapsed_time(&parent_process_timer);
-  printf(">> O tempo total foi de %.1f segundos\n", parent_elapsed_time);
-  
-  return 0;
+void manage_parent()
+{
+    int ps_status;
+    double child_elapsed_time;
+
+    wait(&ps_status);
+    child_elapsed_time = timer_get_elapsed_time(&child_process_timer);
+    printf("> Demorou %.1f segundos, retornou %d\n", child_elapsed_time, WEXITSTATUS(ps_status));
+    fflush(stdout);
+}
+
+int main()
+{
+    signal(SIGUSR1, start_child_timer);
+
+    pid_t process;
+    command_t command;
+
+    struct timeval parent_process_timer;
+    double parent_elapsed_time;
+
+    timer_start(&parent_process_timer);
+
+    while (scanf("%s %s", command.path, command.args) != EOF)
+    {
+        process = fork();
+
+        if (process > 0)
+            manage_parent();
+        else if (process == 0)
+            manage_child(&command);
+        else
+        {
+            printf("> Não foi possível criar um processo.\n");
+            fflush(stdout);
+            exit(1);
+        }
+    }
+
+    parent_elapsed_time = timer_get_elapsed_time(&parent_process_timer);
+    printf(">> O tempo total foi de %.1f segundos\n", parent_elapsed_time);
+    fflush(stdout);
+
+    return 0;
 }
